11d9d34e3094f62ed87b84afd5dc9748400a975f
Yadnesh Devkar <yadneshdevkar9653@gmail.com>
2025-12-06 14:11:43 +0530

    Fix image paths for GitHub Pages using PUBLIC_URL

--- src/Features.js
+++ src/Features.js
@@ -2,19 +2,19 @@ import React from "react";
 import dataJson from "./data.json";
 import "./features.scss";
 
-
 export default function Features({ item, id }) {
   const { features, promo } = dataJson;
-
 
   return (
     <>
       <div className="card-container">
-        <img src={`/images/${item.icon}`} />
+        <img
+          src={`${process.env.PUBLIC_URL}/images/${item.icon}`}
+          alt={item.title}
+        />
         <h2>{item.title}</h2>
         <p>{item.desc}</p>
       </div>
-  
     </>
   );
 }
